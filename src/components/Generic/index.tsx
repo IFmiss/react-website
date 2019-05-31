5b68675e4ffbc7b6fe907af0251ebb5f18ea382c
DavidDai <185098535@qq.com>
2019-05-31 20:45:23 +0800

    在less中使用全局的css变量

--- src/components/Generic/index.tsx
+++ src/components/Generic/index.tsx
@@ -17,12 +17,13 @@ export interface GenericState {}
 export default class GenericComponent<P, S> extends React.Component<P, S> {
   props: P & GenericProps & { children?: any };
   state: S & GenericState;
-  
+
   componentDidMount () {
     console.log('this.props', this.props)
   }
 
   static defaultProps = {
+    prefixClass: Constance.PROJECT_NAME,
     className: `generic-component ${Constance.PROJECT_NAME}`
   }
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import GenericComponent from './../Generic/index'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { INavLists, INavList } from './../../config/nav'
+import classNames from 'classnames'
+import { GenericUtils } from '@dw/d-utils'
 import './nav.less'
 
 interface INavProps {
@@ -11,32 +13,47 @@ interface INavProps {
 interface INavState {}
 
 export default class Nav extends GenericComponent<INavProps, INavState> {
-  public linkClass = (url: string) => {
-    return location.pathname.includes(url) ? 'nav-link active-router-link' : 'nav-link'
+  public NavStyle = {
+    color: '#333'
   }
 
-  componentDidMount () {
-    console.log(this.props)
+  public addHover = () => {
+
+  }
+
+  public removeHover = () => {
+    
+  }
+
+  public changeColor = () => {
+    document.documentElement.style.setProperty(`--color`, GenericUtils.randomColor());
   }
 
   render () {
-    const { lists, className, prefixClass } = this.props
-    const classNames = ({
+    const { lists, prefixClass } = this.props
+    const classes = classNames({
       [`${prefixClass}-nav`]: true
     })
     return (
-      <div className={`${className} react-web-nav`}>
+      <div className={classes}>
         <div className="nav-content">
           <div className="nav-logo">
             <img className="nav-img" src="" alt=""/>
           </div>
-          {
-            lists.map((item: INavList, index: number) => {
-              return (
-                <Link className={this.linkClass(item.link)} to={item.link} key={index}>{item.name}</Link>
-              )
-            })
-          }
+          <div className="nav-lists">
+            {
+              lists.map((item: INavList, index: number) => {
+                return (
+                  <NavLink className="nav-list"
+                          to={item.link}
+                          key={index}
+                          // style={this.NavStyle}
+                          onMouseEnter={this.addHover}>{item.name}</NavLink>
+                )
+              })
+            }
+          </div>
+          <div onClick={this.changeColor}>点击切换颜色</div>
         </div>
       </div>
     )
