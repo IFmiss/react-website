03762ea5748ff438dd45879e4605b5759ba8ef28
DavidDai <185098535@qq.com>
2019-07-27 18:59:00 +0800

    Daudio

--- src/components/DAudio/index.tsx
+++ src/components/DAudio/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useMemo, useLayoutEffect, useRef, useImperativeHandle } from 'react'
 import classNames from 'classnames'
+import Vibrant from 'node-vibrant'
+import ReactDOM from 'react-dom';
 import './d-audio.less'
 
 interface IMusicInfo {
@@ -9,7 +11,7 @@ interface IMusicInfo {
   singer: string;
 }
 
-enum DAudioStyle {
+enum DAudioType {
   // 矩形
   RECT = 1,
 
@@ -17,13 +19,172 @@ enum DAudioStyle {
   CIRCLE,
 }
 
-interface IDAudioProps {}
+enum DAudioPosition {
+  BOTTOM_LEFT = 1,
+  BOTTOM_RIGHT
+}
+
+interface IDAudioProps {
+  position?: DAudioPosition;
+  type?: DAudioType;
+  width?: number;
+  height?: number;
+}
 
 interface IDAudioState {
-  music: IMusicInfo;
+  list: IMusicInfo;
   progress: boolean;
   loop: boolean;
-  style: DAudioStyle;
+  type: DAudioType;
   ended: () => void;
   next: () => void;
 }
+
+const defaultList = {
+  url: '',
+  coverUrl: 'http://www.daiwei.org/sunmmer.jpg',
+  name: 'd-audio player',
+  singer: '未曾遗忘的青春'
+}
+
+const DAudio: React.FC<IDAudioProps> = function (props, ref) {
+  const audioWidth = props.width && props.width > 240 ? props.width : 240
+  const audioHeight = props.height && props.height > 50 ? props.width : 50
+  const audioRef = useRef(null)
+  const [type, setType] = useState<DAudioType>(props.type || DAudioType.CIRCLE)
+  const [list, setList] = useState<IMusicInfo>(defaultList)
+  const [isPlay, setIsPlay] = useState<Boolean>(false)
+  const [loading, setLoading] = useState<Boolean>(false)
+
+  const imageColor = useMemo(() => {
+    Vibrant.from(list.coverUrl).getPalette()
+    .then((palette: any) => {
+      const {r, g, b} = palette.LightMuted
+      return {
+        defaultColor: `rgba(${r}, ${g}, ${b}, 1)`,
+        circleBorderColor: `rgba(${r}, ${g}, ${b}, 0.36)`,
+        progressLeftColor: `rgba(${r}, ${g}, ${b}, 0.12)`,
+        progressRightColor: `rgba(${r}, ${g}, ${b}, 0.66)`,
+      }
+    }).catch((err) => {
+      return {
+        defaultColor: `rgba(255, 56, 56, 1)`,
+        circleBorderColor: `rgba(255, 56, 56, 0.36)`,
+        progressLeftColor: `rgba(255, 56, 56, 0.12)`,
+        progressRightColor: `rgba(255, 56, 56, 0.66)`,
+      }
+    })
+  }, [list.coverUrl])
+
+  // style
+  const circleStyle = {
+    width: audioWidth,
+    height: audioHeight
+  }
+
+  const blurStyle = {
+    backgroundImage: `url(${list.coverUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center'
+  }
+
+  const progressStyle = {
+    backgroundImage: `linear-gradient(to right, ${imageColor.progressLeftColor} 30%, ${imageColor.progressRightColor})`
+  }
+
+  // const imageColorL
+
+  const selfClass = classNames({
+    [`d-audio`]: true,
+  })
+
+  const classString = classNames({
+    selfClass,
+    [`bottom-left`]: props.position === DAudioPosition.BOTTOM_LEFT,
+    [`bottom-right`]: props.position === DAudioPosition.BOTTOM_RIGHT,
+    [`circle`]: type === DAudioType.CIRCLE,
+    [`rect`]: type === DAudioType.RECT,
+  })
+
+  const classPlayPause = classNames({
+    [`d-audio-play-pause`]: true,
+    [`play`]: isPlay,
+    [`pause`]: !isPlay
+  })
+
+  const classLoading = classNames({
+    [`d-audio-loading`]: true,
+    [`active`]: loading
+  })
+
+  const start = (musicList: IMusicInfo) => {
+    setList((list) => list = musicList)
+    play()
+  }
+
+  const play = () => {
+    console.log('play')
+    audioRef.current
+  }
+
+  const next = () => {
+    console.log('play')
+    audioRef.current
+  }
+
+  const pause = () => {
+    console.log('play')
+    audioRef.current
+  }
+
+  useImperativeHandle(ref, () => ({
+    start,
+    next,
+    play,
+    pause,
+  }));
+
+  return (
+    <div className={classString}>
+      <div className={`${selfClass}-circle`}
+           title={`${list.name} - ${list.singer}`}
+           style={circleStyle}>
+        <img src={list.coverUrl}/>
+        <div className={`${selfClass}-range`}></div>
+      </div>
+      <div className={`${selfClass}-detail`}>
+        <div className={`${selfClass}-detail-config`}>
+          <div className={classPlayPause}></div>
+          <div className="d-audio-next"></div>
+        </div>
+        <div className={`${selfClass}-detail-info`}>
+          <div className="music-name">{list.name}</div>
+          <div className="music-singer">{list.singer}</div>
+        </div>
+      </div>
+      <div className={`${selfClass}-blur-bg`} style={blurStyle}></div>
+      <div className={classLoading}></div>
+      <div className={`${selfClass}-progress`} style={progressStyle}></div>
+      <audio src={list.url} ref={audioRef}></audio>
+    </div>
+  )
+}
+
+const DuadioComponent = React.forwardRef(DAudio)
+
+function newInstance(props: IDAudioProps) {
+  const DAudioRef = React.createRef<IDAudioState>();
+
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(<DuadioComponent {...props} ref={DAudioRef}/>, div)
+
+  const destroy = () => {
+    ReactDOM.unmountComponentAtNode(div);
+    (div.parentNode as HTMLDivElement ).removeChild(div);
+  }
+
+  const { error, finish, start, reset } = DAudioRef.current as IDAudioState;
+}
+
+export default newInstance
---
+++ src/components/Dialog/index.tsx
@@ -0,0 +1,73 @@
+
+import * as ReactDOM from 'react-dom';
+import React from 'react'
+import classNames from 'classnames';
+import { PROJECT_NAME } from './../../config/constance'
+export interface IAlertOption {
+  title?: string;
+  desc?: string;
+  btnText?: string;
+  onClose?: () => void;
+  onBtnClick?: () => void;
+}
+
+export default function alert (
+  title = 'title',
+  desc = 'desc',
+  btnText = '确定',
+  onClose = () => {},
+  onBtnClick = () => {}
+) {
+  console.log(title)
+  console.log(desc)
+  console.log(btnText)
+  console.log(onClose)
+  console.log(onBtnClick)
+  const div: any = document.createElement('div');
+  document.body.appendChild(div);
+
+  function close () {
+    onClose && onClose()
+    ReactDOM.unmountComponentAtNode(div);
+    if (div && div.parentNode) {
+      div.parentNode.removeChild(div);
+    }
+  }
+
+  function confirm () {
+    onBtnClick && onBtnClick()
+    ReactDOM.unmountComponentAtNode(div);
+    if (div && div.parentNode) {
+      div.parentNode.removeChild(div);
+    }
+  }
+
+  function handleTouchMove (e: any) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  const classString = classNames({
+    [`${PROJECT_NAME}-comp-dialog`]: true
+  })
+  // const { title, btnText, desc } = props
+
+  ReactDOM.render(
+    <div className={classString}>
+      <div className={classString} onTouchMove={handleTouchMove}>
+        <div className={`${classString}-mask`}></div>
+        <div className={`${classString}-content`}>
+          {
+            title ? (<div className={`${classString}-content-title`}>{title}</div>) : null
+          }
+          {
+            desc ? (<div className={`${classString}-content-desc`}>{desc}</div>) : null
+          }
+          {
+            btnText ? (<div className={`${classString}-content-button`} onClick={confirm}>{btnText}</div>) : null
+          }
+        </div>
+      </div>
+    </div>
+  , div)
+}
