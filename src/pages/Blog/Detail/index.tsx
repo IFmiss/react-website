acc9e8fc842c4ab21a24c3226d309ebbbb94472c
ifmiss <185098535@qq.com>
2019-09-25 23:40:51 +0800

    优化详情页内容

--- src/pages/Blog/Detail/index.tsx
+++ src/pages/Blog/Detail/index.tsx
@@ -4,6 +4,7 @@ import { IStore } from './../../../store/types'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import classNames from 'classnames'
 import './detail.less'
+import './../../../style/high-default.less'
 import {
   getBlogDetail
 } from './../action'
@@ -11,6 +12,22 @@ import { IBlogListCategorieOrTag } from '../../../components/BlogList'
 import { PROJECT_NAME, DEFAULT_BLOG_DETAIL } from './../../../config/constance'
 import SiderWarp from './../../../components/SiderWarp'
 import * as UrlUtils from 'd-utils/lib/urlUtils'
+const marked = require('marked');
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: function(code: any) {
+    return require('highlight.js').highlightAuto(code).value;
+  },
+  pedantic: false,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+});
 
 interface IBlogDetailProps {
 }
@@ -44,10 +61,10 @@ const BlogDetail: React.FC<IBlogDetailProps> = () => {
   }, [])
   return (
     <section className={classString}>
-      <div className={`${classString}-content`}>
-        {detail.content}
+      <div className={`${classString}-content`}
+           dangerouslySetInnerHTML = {{__html: marked(detail.content)}}>
       </div>
-      {/* <SiderWarp show={false} switchTop="20px" type="auto" ref={}>
+      <SiderWarp show={false} switchTop="20px" type="auto">
         <h4 className="sider-title">目录</h4>
         <div>1111</div>
         <div>1111</div>
@@ -55,7 +72,7 @@ const BlogDetail: React.FC<IBlogDetailProps> = () => {
         <div>1111</div>
         <div>1111</div>
         <div>1111</div>
-      </SiderWarp> */}
+      </SiderWarp>
     </section>
   )
 }
